312505f42c3aadc5977fb7e04bf33dac8716aa20
Jc <39649036+KuroganeYuu@users.noreply.github.com>
2024-04-21 21:16:18 +0800

    Update db.sql
    removed erros

--- database/db.sql
+++ database/db.sql
@@ -1,5 +1,3 @@
---WIP
-
 CREATE TABLE Books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -8,17 +6,16 @@ CREATE TABLE Books (
     publication_year INT,
     status ENUM('Available', 'Borrowed') DEFAULT 'Available'
 );
---borrowed_id from borrowings? to show in user profile  <----!
+
 CREATE TABLE Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     usn VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    pwd VARCHAR(255) NOT NULL,
-    -- Add other relevant user information here
+    pwd VARCHAR(255) NOT NULL
 );
---Transactions/borrowings still need testing <-------!
+
 CREATE TABLE Borrowings (
     id INT AUTO_INCREMENT PRIMARY KEY,
     book_id INT,
@@ -29,13 +26,12 @@ CREATE TABLE Borrowings (
     FOREIGN KEY (book_id) REFERENCES Books(id),
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
---Prob just changing this to admin
+
 CREATE TABLE Librarians (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     usn VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    pwd VARCHAR(255) NOT NULL, -- Encrypted password
-    -- Add other relevant librarian information here
+    pwd VARCHAR(255) NOT NULL
 );
