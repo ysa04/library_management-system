8e19495f863f37fede1b70191195bea4254d3b93
unknown <gatongaymariaisabel@gmail.com>
2024-06-02 22:22:50 +0800

    additonal update

--- backend/view/student/bookNavigate.php
+++ backend/view/student/bookNavigate.php
@@ -6,36 +6,113 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/frontend/css/student.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>student Home Page</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Symbols+Outlined:opsz, wght, FILL, GRAD@48,400,0,0">
+    
+    <title>Student Home Page</title>
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+        .book-card {
+            margin-top: 50px;
+            margin-bottom: 50px;
+        }
+        .book-card img {
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
 </head>
 <body>
-<?php include 'studentNavbar.php'; ?> 
-<?php
-// Database connection parameters
-$host = "localhost";
-$username = "root";
-$password = "ysa_2024_gatongay";
-$database = "library";
+        <!-- PHP for session data -->
+        <?php
+    session_start();
+    if (!isset($_SESSION['usn']) || !isset($_SESSION['first_name']) || !isset($_SESSION['last_name'])) {
+        header("Location: studentlogin.php");
+        exit();
+    }
+    $usn = $_SESSION['usn'];
+    $name = $_SESSION['first_name'];
+    $surName = $_SESSION['last_name'];
+    require_once 'logout.php';
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['logout'])) {
+        logout();
+    }
+    ?>
+    <!-- Navigation bar -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img class="top-icon" src="/frontend/img/header-book-icon.png" width="30" height="30" alt=""/>
+                <p>e-Library</p>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CONTACT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PROFILE</a>
+                    </li>
+                </ul>
+                <div style="display: flex; color: white; margin-top: 15px;">
+                    <i style="margin-top: 4px;" class="fa-solid fa-user"></i>
+                    <p>&nbsp;<?php echo $name; ?>,</p>
+                    <p>&nbsp;<?php echo $surName; ?></p> <br/>
+                    <p>&nbsp;&nbsp;USN: <?php echo $usn; ?></p>
+                    <form method="post" action="/backend/view/index.php">
+                        <input type="hidden" name="logout" value="true">
+                        <button class="logout-button" type="submit">logout</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
 
-// Attempt to establish a connection to the MySQL database
-$con = new mysqli($host, $username, $password, $database);
+    
+    <div style="background-color: rgb(191, 222, 234); border-radius: 5px; position: relative;" class="container">
+        <div class="main-content">
+            <h4>"If you study to remember, you will forget; But if you study to understand you will remember"</h4>
+            <h1>BOOK CATEGORIES</h1>
+            
+    <!-- Main Content -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="card book-card">
+                    <div class="card-body">
+                        <?php
+                        // Database connection parameters
+                        $host = "localhost";
+                        $username = "root";
+                        $password = "ysa_2024_gatongay";
+                        $database = "library";
 
-// Check if the connection was successful
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-}
+                        // Attempt to establish a connection to the MySQL database
+                        $con = new mysqli($host, $username, $password, $database);
 
-// Check if the book ID is specified in the query parameters
-if (isset($_GET['book_id'])) {
-    // Retrieve the book ID from the query parameter
-    $book_id = $_GET['book_id'];
+                        // Check if the connection was successful
+                        if ($con->connect_error) {
+                            die("Connection failed: " . $con->connect_error);
+                        }
 
-    // Prepare and execute a query to fetch details of the specified book
-    $query = "SELECT book_id, title, author, summary, genre, book_count, publication_year, stat, image_name, image_data FROM books WHERE book_id = ?";
-    $stmt = $con->prepare($query);
-    $stmt->bind_param("i", $book_id);
-    $stmt->execute();
-    $result = $stmt->get_result();
+                        // Check if the book ID is specified in the query parameters
+                        if (isset($_GET['book_id'])) {
+                            // Retrieve the book ID from the query parameter
+                            $book_id = $_GET['book_id'];
+
+                            // Prepare and execute a query to fetch details of the specified book
+                            $query = "SELECT book_id, title, author, summary, genre, book_count, publication_year, stat, image_name, image_data FROM books WHERE book_id = ?";
+                            $stmt = $con->prepare($query);
+                            $stmt->bind_param("i", $book_id);
+                            $stmt->execute();
+                            $result = $stmt->get_result();
 
     // Check if the book is found
     if ($result->num_rows == 1) {
@@ -56,7 +133,6 @@ if (isset($_GET['book_id'])) {
         echo "<h5>Summary:</h5>";
         echo "<p>" . $row['summary'] . "</p>";
         echo "</div>";
-
         
         echo "</div>";
 
@@ -68,12 +144,22 @@ if (isset($_GET['book_id'])) {
     echo "<p>Book ID not specified.</p>";
 }
 
-// Close the database connection
-$con->close();
-?>
-   
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+                        // Close the database connection
+                        $con->close();
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>    
+    </div>         
+    
 
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
+<!-- Footer -->
+<?php include '../../../frontend/components/student/footer.html'; ?>
 </html>
--- backend/view/student/student_home.php
+++ backend/view/student/student_home.php
@@ -6,13 +6,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/frontend/css/student.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Symbols+Outlined:opsz, wght, FILL, GRAD@48,400,0,0">
+    <link rel="stylesheet" href="student_chatbox.css">
+    <script src="student_chatbox.js" defer></script>
     <title>student Home Page</title>
     <style>
  
 
         .book-cover {
             flex: 0 0 calc(100% - 20px); /* Adjust the width for covers per row */
-            margin: 15px 0px px 50px; /* Adjust the margin as needed */
+            margin: 15px 0px 50px; /* Adjust the margin as needed */
             max-width: 250px; /* Set a maximum width for the book cover */
             height: auto; /* Automatically adjust the height to maintain aspect ratio */
         }
@@ -22,59 +25,73 @@
 <body>
 <?php include 'studentNavbar.php'; ?> 
 
-   <div style="background-color: rgb(191, 222, 234); border-radius: 5px; position: relative;" class="container">
-
-    <div class="main-content ">
-    <h4>"If you study to remember, you will forget; But if you study to understand you will remember" </h4>
-    <h1>BOOK CATEGORIES</h1>
-
-          <!-- where is toggleFilter use for? -->
-    <form class="nav-search" onclick="toggleFilter()" id="filterForm">
-        <div class="">
-            <input type="text" name="title" id="titleInput" placeholder="Title">
-            <input type="text" name="author" id="authorInput" placeholder="Author">
-            <input type="text" name="year" id="yearInput" placeholder="Publication Year">
-            <select name="genre" id="genreSelect">
-                <option value="">Select Genre</option>
-                <option value="Business">Business</option>
-                <option value="Nursing">Nursing</option>
-                <option value="IT">IT</option>
-                <option value="Fantasy">Fantasy</option>
-                <option value="Comedy">Comedy</option>
-                <!-- Add more genre options as needed -->
-            </select>
-            <select name="status" id="statusSelect">
-                <option value="">Select Status</option>
-                <option value="Available">Available</option>
-                <option value="Borrowed">Borrowed</option>
-            </select>
-            <button class="search-button" type="button" onclick="applyFilter()">Search</button> <!-- Use type="button" to prevent form submission -->
+!-- Main content -->
+<div style="background-color: rgb(191, 222, 234); border-radius: 5px; position: relative;" class="container">
+        <div class="main-content">
+            <h4>"If you study to remember, you will forget; But if you study to understand you will remember"</h4>
+            <h1>BOOK CATEGORIES</h1>
+            <form class="nav-search" id="filterForm">
+                <div>
+                    <input type="text" name="title" id="titleInput" placeholder="Title">
+                    <input type="text" name="author" id="authorInput" placeholder="Author">
+                    <input type="text" name="year" id="yearInput" placeholder="Publication Year">
+                    <select name="genre" id="genreSelect">
+                        <option value="">Select Genre</option>
+                        <option value="Business">Business</option>
+                        <option value="Nursing">Nursing</option>
+                        <option value="IT">IT</option>
+                        <option value="Fantasy">Fantasy</option>
+                        <option value="Comedy">Comedy</option>
+                    </select>
+                    <select name="status" id="statusSelect">
+                        <option value="">Select Status</option>
+                        <option value="Available">Available</option>
+                        <option value="Borrowed">Borrowed</option>
+                    </select>
+                    <button class="search-button" type="button" onclick="applyFilter()">Search</button>
+                </div>
+            </form>
         </div>
-    </form>
-   </div>
 
-   <div class="gallery">
-        <?php include_once "searchhome.php"; ?>
+        <!-- Book gallery -->
+        <div class="gallery">
+            <?php include_once "searchhome.php"; ?>
+        </div>
+        
+        <!-- Chatbot section -->
+        <button class="chatbot-toggler">
+            <img src="/frontend/img/chatbot.png" alt="Chatbot Icon" class="chatbot-img">
+            <span class="material-symbols-outlined">close</span>
+        </button>
+        <div class="chatbot">
+            <header>
+                <h2>Amaia</h2>
+                <span class="close-btn material-symbols-outlined">close</span>
+            </header>
+            <ul class="chatbox">
+                <li class="chat incoming">
+                    <span class="material-symbols-outlined">smart_toy</span>
+                    <p>Hi i'm Amaia <br> How can I help you today?</p>
+                </li>
+            </ul>
+            <div class="chat-input">
+                <textarea placeholder="Enter a message..."></textarea>
+                <span id="send-btn" class="material-symbols-outlined">send</span>
+            </div>
+        </div>
     </div>
-     
-     <div class="chatbot-div">
-      <img src="/frontend/img/chatbot.jpg" alt="chatbot" width="65" height="65"/>
-      <a href="/">Chat with Amaia</a>  
-     </div>
 
-  </div>
+    <!-- Footer -->
+    <?php include '../../../frontend/components/student/footer.html'; ?>
 
-<?php
-include '../../../frontend/components/student/footer.html';
-?>
+    <!-- Scripts -->
+    <script src="/frontend/js/bookNavigate.js"></script>
+    <script src="/frontend/js/retrieve_data.js"></script>
+    <script src="/frontend/js/filtersearch.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
-
+    <!-- Student Chatbox JS -->
     
-<script src="/frontend/js/bookNavigate.js"></script>
-<script src="/frontend/js/retrieve_data.js"></script>
-<script src="/frontend/js/filtersearch.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-
 </body>
 </html>
